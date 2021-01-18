edc5cf94473294ad349587a084a8a13673c4a3fd
omar quinones <omarquinones@omars-mbp.attlocal.net>
2021-01-17 19:15:43 -0800

    Fixed classNames and ids

--- src/Components/survey.jsx
+++ src/Components/survey.jsx
@@ -20,7 +20,7 @@ class Survey extends Component {
                 <section id="form-section">
                     <form id="survey-form">
                         <div class="basic-info">
-                            <label for="name" id="name-label">Name</label><br/>
+                            <label for="name" id="name-label" className="input-desc">Name</label><br/>
                             <input
                                 type="text"
                                 id="name"
@@ -29,7 +29,7 @@ class Survey extends Component {
                         </div>
 
                         <div class="basic-info">
-                            <label for="email" id="email-label">Email</label><br/>
+                            <label for="email" id="email-label" className="input-desc">Email</label><br/>
                             <input 
                                 type="email"
                                 id="email"
@@ -38,7 +38,7 @@ class Survey extends Component {
                         </div>
                         
                         <div class="basic-info">
-                            <label for="age" id="number-label">Age <small>(Optional)</small> </label><br/>
+                            <label for="age" id="number-label" className="input-desc">Age <small>(Optional)</small> </label><br/>
                             <input
                                 type="number"
                                 id="number"
@@ -61,23 +61,22 @@ class Survey extends Component {
                             </select>
                         </div>
 
-                        <div id="btns" class="basic-info">
-                            {/* <p id="best-mf">Would you reccommend freeCodeCamp to a friend?</p> */}
+                        <div id="btns" className="basic-info">
                             <div id="radio">
-                                <label id="dropdown-label">Would you reccommend freeCodeCamp to a friend?</label><br/>
+                                <label id="rec">Would you reccommend freeCodeCamp to a friend?</label><br/>
                                 <input type="radio" name="raiod-btn" value="Burning_Man"/>
-                                <label id="Burning_Man" for="Burning_Man">Definitely</label>
+                                <label id="Definitely" for="Definitely" className="btn-text">Definitely</label>
                                 <br/>
                                 <input type="radio" name="raiod-btn" value="EDC_Las_Vegas"/>
-                                <label id="EDC_Las_Vegas" name="EDC_Las_Vegas">Maybe</label>
+                                <label id="EDC_Las_Vegas" name="EDC_Las_Vegas" className="btn-text">Maybe</label>
                                 <br/>
                                 <input type="radio" name="raiod-btn" value="Ultra"/>
-                                <label id="Ultra" name="raiod-btn">Not sure</label>
+                                <label id="Ultra" name="radio-btn" className="btn-text">Not sure</label>
                             </div>
                         </div>
 
                         <div class="basic-info">
-                            <label id="dropdown-label">What is your favorite feature of freeCodeCamp?</label><br/>
+                            <label id="feature">What is your favorite feature of freeCodeCamp?</label><br/>
                             <select id="dropdown" placeholder="Select an option">
                                 {/* <div> */}
                                     <option value="Student">Challenges</option>
@@ -90,31 +89,30 @@ class Survey extends Component {
 
 
                         <div id="btns" class="basic-info">
-                            {/* <p id="Fav-djs">What would you like to see improved? <small>(check all that apply)</small></p> */}
                             <div id="djs">
-                                <label id="dropdown-label">What would you like to see improved? <small>(check all that apply)</small></label><br/>
+                                <label  id="improved">What would you like to see improved? <small>(check all that apply)</small></label><br/>
                                 <input type="checkbox" name="Deadmau5" value="Deadmau5"/>
-                                <label for="Deadmau5">Front-end Projects</label><br/>
+                                <label for="Deadmau5" className="btn-text">Front-end Projects</label><br/>
                                 <input type="checkbox" name="W&W" value="W&W"/>
-                                <label for="W&W">Back-end Projects</label><br/>
+                                <label for="W&W" className="btn-text">Back-end Projects</label><br/>
                                 <input type="checkbox" name="RUFUS DU SOL" value="RUFUS DU SOL"/>
-                                <label for="RUFUS DU SOL">Data Visualization</label><br/>
+                                <label for="RUFUS DU SOL" className="btn-text">Data Visualization</label><br/>
                                 <input type="checkbox" name="Kygo" value="Kygo"/>
-                                <label for="Kygo">Challenges</label><br/>
+                                <label for="Kygo" className="btn-text">Challenges</label><br/>
                                 <input type="checkbox" name="Jauz" value="Jauz"/>
-                                <label for="Jauz">Open Source Community</label><br/>
+                                <label for="Jauz" className="btn-text">Open Source Community</label><br/>
                                 <input type="checkbox" name="Seven Lions" value="Seven Lions"/>
-                                <label for="Seven Lions">Gitter help rooms</label><br/>
+                                <label for="Seven Lions" className="btn-text">Gitter help rooms</label><br/>
                                 <input type="checkbox" name="Seven Lions" value="Seven Lions"/>
-                                <label for="Seven Lions">Videos</label><br/>
+                                <label for="Seven Lions" className="btn-text">Videos</label><br/>
                                 <input type="checkbox" name="Seven Lions" value="Seven Lions"/>
-                                <label for="Seven Lions">City Meetups</label><br/>
+                                <label for="Seven Lions" className="btn-text">City Meetups</label><br/>
                                 <input type="checkbox" name="Seven Lions" value="Seven Lions"/>
-                                <label for="Seven Lions">Wiki</label><br/>
+                                <label for="Seven Lions" className="btn-text">Wiki</label><br/>
                                 <input type="checkbox" name="Seven Lions" value="Seven Lions"/>
-                                <label for="Seven Lions">Forum</label><br/>
+                                <label for="Seven Lions" className="btn-text">Forum</label><br/>
                                 <input type="checkbox" name="Seven Lions" value="Seven Lions"/>
-                                <label for="Seven Lions">Additional Courses</label><br/>
+                                <label for="Seven Lions" className="btn-text">Additional Courses</label><br/>
                             </div>
                         </div>
                         <div class="basic-info">
