71f88bf47868a06949d7ea70fbf7d185be4be052
omar quinones <omarquinones@omars-mbp.attlocal.net>
2021-02-10 14:41:39 -0800

    Updates

---
+++ precache-manifest.31efd9bcbd265b027b45207a4c9dc449.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60c40f1aca302071046f8543e4e5e29a",
+    "url": "/freecodecamp_surveyform/index.html"
+  },
+  {
+    "revision": "f694d43ecabee00f5ba9",
+    "url": "/freecodecamp_surveyform/static/css/main.59d58582.chunk.css"
+  },
+  {
+    "revision": "b886c946e8fff733a216",
+    "url": "/freecodecamp_surveyform/static/js/2.82a7bed4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/freecodecamp_surveyform/static/js/2.82a7bed4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f694d43ecabee00f5ba9",
+    "url": "/freecodecamp_surveyform/static/js/main.f4ec07d0.chunk.js"
+  },
+  {
+    "revision": "7f10af343216f21d5f43",
+    "url": "/freecodecamp_surveyform/static/js/runtime-main.1579bc69.js"
+  }
+]);
